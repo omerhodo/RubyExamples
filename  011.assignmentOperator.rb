37226820ebb5c665bbfca2a8693a124607d3a535
Ömer Hodo <omerhodo@gmail.com>
2021-02-26 15:20:35 +0300

    011 added

---
+++  011.assignmentOperator.rb
@@ -0,0 +1,15 @@
+x=1 #atamayı sağdan sola yapar
+# += kendi değerini sağdaki değer ile toplar tekrar yazar
+puts x +=2
+
+# -= kendi değerini sağdaki değer ile çıkarır tekrar yazar
+puts x-=2
+
+# *= kendi değerini sağdaki değer ile çarpar tekrar yazar
+puts x*=4
+
+# /= kendi değerini sağdaki değer ile böler tekrar yazar
+puts x/3.to_f
+
+x=1
+puts x # x'in değeri tekrar güncellenebilir
